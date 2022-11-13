3d8759b546889981efebcbf8cd42c1d1e9b60578
DimonIPymba <nazvanie5486@gmail.com>
2022-11-13 17:18:03 +0200

    Добавьте файлы проекта.

---
+++ Printer/Printerlol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program;
+
+public class Printerlol : Printer
+{
+    public override void Print(string value)
+    {
+        var prevColor = Console.ForegroundColor;
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        Console.WriteLine(value.GetType());
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+
+        base.Print(value);
+
+        Console.ForegroundColor = prevColor;
+    }
+}
---
+++ Printer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program;
+
+class Program
+{
+    static void Main()
+    {
+        Printer Printerlol = new Printerlol();
+        Printerlol.Print("test");
+    }
+}
